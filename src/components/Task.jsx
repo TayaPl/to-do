5b1e774a98cc1f367d7de25ace19e5d80b75a72b
Taya Pleskacheva <qa.important@gmail.com>
2022-09-09 19:43:08 +0300

    Добавлена функция нажатия ToDo кнопок

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "./UI/button/Button.jsx";
 import "../styles/Task.css";
 
 const Task = ({ children, ...props }) => {
+  const [isActive, setActive] = useState("false");
+
+  const handleToggle = () => {
+    setActive(!isActive);
+  };
+
   return (
     <div id="task" className="task">
       <div className="task_container">
-        <Button classNames="task_button"></Button>
-        <div className="task_text">Я задача</div>
+        <Button
+          classNames={(!isActive ? "task_button__active" : "") + " task_button"}
+          onClick={handleToggle}
+        ></Button>
+        <div className={(!isActive ? "task_text__active" : "") + " task_text"}>
+          Я задача
+        </div>
       </div>
     </div>
   );
